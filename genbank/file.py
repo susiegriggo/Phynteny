aa88a8c23931e6c2e142b4981f2a16f41cfd12d7
susie.griggo <susie.grigson@flinders.edu.au>
2022-12-21 00:30:43 +1030

    genbank_template

--- genbank/file.py
+++
@@ -1,5 +0,0 @@
-from genbank.file import File
-from genbank_template.locus import Locus
-
-class File(File, locus=Locus):
-	pass
--- phynteny/__init__.py
+++ phynteny/__init__.py
@@ -1,1 +1,3 @@
-#do
+from .format_data import encode_feature
+
+__all__ = ['encode_feature'] #use this to add modules to the repository 
