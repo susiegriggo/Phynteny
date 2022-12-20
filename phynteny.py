227ea4c0cbb9298c28a0666378a25d275cfb7498
Susie Grigson <susie.grigson@flinders.edu.au>
2022-12-20 16:14:53 +1030

    nsetup

---
+++ phynteny.py
@@ -0,0 +1,46 @@
+#script to run phynteny from 
+
+#include argparser etc 
+
+
+from phyteny import (
+
+#put modules which need importing here 
+
+
+        )
+
+
+
+__author__ = "Susanna Grigson" 
+__maintainer__ = "Susanna Grigson" 
+__email__ = "susie.grigson@flinders.edu.au" 
+__status__ = "development" 
+
+
+# Set Parameters 
+# ------------------------- 
+
+
+
+
+
+def main(): 
+
+
+    #Setup argument parser for the tool 
+    # ---------------------------------
+
+    
+
+    #validate the arguments 
+    #i.e have restrictions on what is allowed 
+
+
+    #include a logger 
+
+
+
+
+if __name__ == "__main__": 
+    main() 
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+
+import setuptools 
+
+
+with open("README.md", "r") as f: 
+    long_description = f.read() 
+
+
+packages = setuptools.find_packages() 
