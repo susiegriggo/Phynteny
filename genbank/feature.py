8494a4644339e7d1a587ef3d096dea25d3b963e2
Susie Grigson <susie.grigson@flinders.edu.au>
2022-12-20 20:32:00 +1030

    genbank

---
+++ genbank/feature.py
@@ -0,0 +1,10 @@
+from genbank.feature import Feature
+
+
+class Feature(Feature):
+	def foo(self):
+		'''this adds a custom definition to the Feature class'''
+		return 'bar'
+
+
+
---
+++ genbank/file.py
@@ -0,0 +1,5 @@
+from genbank.file import File
+from genbank_template.locus import Locus
+
+class File(File, locus=Locus):
+	pass
