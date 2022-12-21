bfa8458908a02da5ca1984aae92b04ae44666ef3
susiegriggo <susie.grigson@flinders.edu.au>
2022-12-21 13:17:36 +1030

    nupdate environment

--- parse_genbank.py
+++ parse_genbank.py
@@ -4,5 +4,11 @@ Script to parse genbank output
 
 from genbank.file import File
 
-File('test_data/pharokka.gbk')
-print('hi')
+gbank = File('test_data/pharokka.gbk')
+
+print(gbank)
+
+#parse in the various features etc
+#think about the data structure used to store each prophage
+
+
