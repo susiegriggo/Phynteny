13f2575095737e749f4de516b2cae81e3c43e848
Susie Grigson <susie.grigson@flinders.edu.au>
2023-01-20 21:13:16 +1030

    generate training data

--- phynteny/merge_phrog_annotations.py
+++ phynteny/merge_phrog_annotations.py
@@ -4,7 +4,7 @@ Add mmseqs annotations into phispy annotations
 
 #imports 
 import glob2
-from Bio import SeqIO
+from Bio import SeqIO, bgzf 
 import re
 import gzip
 import mmseqs_output
@@ -18,8 +18,6 @@ for d in directories:
     zipped_gdict = [i for i in e if i[-6:] == 'gbk.gz'] 
     
     for file in zipped_gdict: 
-        
-        print(file, flush = True)  
 
         #read in the genbank file 
         with gzip.open(file, 'rt') as handle: 
@@ -34,32 +32,38 @@ for d in directories:
 
         if len(mmseqs_fetch) > 0: 
 
-            mmseqs_output  = mmseqs_output.get_mmseqs(mmseqs_fetch[0])
-            phrogs = mmseqs_output.filter_mmseqs(mmseqs_output)
-            genbank_name = '/home/grig0076/scratch/phispy_phrogs/GCA/' +  file_parts[8] + '/' + file_parts[9] + '/' + file_parts[10] + '/'  + genbank_parts[0] + '_' + genbank_parts[1] + '_phrogs_' + genbank_parts[3]
-            handle = gzip.open(genbank_name, 'wt')
+            annotations  = mmseqs_output.get_mmseqs(mmseqs_fetch[0])
+            
+            if len(annotations) > 0: 
+                
+                phrogs = mmseqs_output.filter_mmseqs(annotations)
+                genbank_name = '/home/grig0076/scratch/phispy_phrogs/GCA/' +  file_parts[8] + '/' + file_parts[9] + '/' + file_parts[10] + '/'  + genbank_parts[0] + '_' + genbank_parts[1] + '_phrogs_' + genbank_parts[3]
+                
+                #handle = gzip.open(genbank_name, 'wt')
 
-            #loop through each prophage
-            for key in gb_keys: 
+                #loop through each prophage
+                for key in gb_keys: 
 
-                #get a prophage
-                this_prophage = gb_dict.get(key) 
+                    #get a prophage
+                    this_prophage = gb_dict.get(key) 
 
-                #get the cds 
-                cds = [i for i in this_prophage.features if i.type == 'CDS']
+                    #get the cds 
+                    cds = [i for i in this_prophage.features if i.type == 'CDS']
 
-                #replace the phrogs 
-                for c in cds: 
+                    #replace the phrogs 
+                    for c in cds: 
 
-                    if 'protein_id' in c.qualifiers.keys(): 
+                        if 'protein_id' in c.qualifiers.keys(): 
 
-                        pid = c.qualifiers.get('protein_id')
+                            pid = c.qualifiers.get('protein_id')
 
-                        if phrogs.get(pid[0]) == None: 
-                            c.qualifiers['phrog'] = 'No_PHROG'
-                        else: 
-                            c.qualifiers['phrog'] = phrogs.get(pid[0])
+                            if phrogs.get(pid[0]) == None: 
+                                c.qualifiers['phrog'] = 'No_PHROG'
+                            else: 
+                                c.qualifiers['phrog'] = phrogs.get(pid[0])
 
-            #write to the genbank file 
-            SeqIO.write(gb_dict.get(key), handle, 'genbank')
-            handle.close() 
+                #write to the genbank file 
+                print(genbank_name, flush = True)  
+                with gzip.open(genbank_name, 'wt') as handle: 
+                    SeqIO.write(gb_dict.get(key), handle, 'genbank')
+                handle.close() 
