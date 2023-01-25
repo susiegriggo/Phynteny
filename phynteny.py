e7cbaf822aab5f7bf7a7daf1fb3a14ebce819680
susiegriggo <susie.grigson@flinders.edu.au>
2023-01-25 14:23:00 +1030

    inputting file

--- phynteny.py
+++ phynteny.py
@@ -1,46 +1,44 @@
-#script to run phynteny from 
+#!/usr/bin/env python3
 
-#include argparser etc 
+"""
+Phynteny: synteny-based annotation of phage genes
+"""
+import argparse
+import sys
+from phynteny import handle_genbank
+from phynteny import format_data
+from phynteny import predict
 
+__author__ = "Susanna Grigson"
+__maintainer__ = "Susanna Grigson"
+__license__ = "MIT"
+__version__ = "0"
+__email__ = "susie.grigson@flinders.edu.au"
+__status__ = "development"
 
-from phyteny import (
+parser = argparse.ArgumentParser(description='Phynteny: synteny-based annotation of phage genes',
+                                 formatter_class=RawTextHelpFormatter)
 
-#put modules which need importing here 
+parser = argparse.add_argument('infile', type=is_valid_file, help='input file in genbank format')
+parser.add_argument('-o', '--outfile', action='store', default=sys.stdout, type=argparse.FileType('w'),
+                    help='where to write the output genbank file')
+parser.add_argument('-m', '--model', action='store', help='Path to custom LSTM model', deafult='nothing')
+parser.add_argument('-V', '--version', action='version', version=__version__)
 
+args = parser.parse_args()
 
-        )
+gb_dict = handle_genbank.get_genbank(args.infile)
+if not gb_dict:
+    sys.stdout.write("Error: no sequences found in genbank file\n")
+    sys.exit()
 
+# Run Phynteny
+# ---------------------------------------------------
 
+# format the genbank file - file to make predictions
+this_phage = format_data.extract_features(gb_dict)
+data = format_data.generate_prediction(this_phage)
 
-__author__ = "Susanna Grigson" 
-__maintainer__ = "Susanna Grigson" 
-__email__ = "susie.grigson@flinders.edu.au" 
-__status__ = "development" 
+predict.predict(data,) #how to instantiate the default model
 
-
-# Set Parameters 
-# ------------------------- 
-
-
-
-
-
-def main(): 
-
-
-    #Setup argument parser for the tool 
-    # ---------------------------------
-
-    
-
-    #validate the arguments 
-    #i.e have restrictions on what is allowed 
-
-
-    #include a logger 
-
-
-
-
-if __name__ == "__main__": 
-    main() 
+# write output to a genbank file
---
+++ phynteny/predict.py
@@ -0,0 +1,29 @@
+"""
+Make predictions using LSTM model
+"""
+
+import tensorflow as tf
+
+
+def predict(X, model):
+    """
+    Predict missing genes using LSTM model
+    param X: phage data formatted into matrix
+    param model: trained LSTM model
+    return yhat: predicted functional categories
+    """
+
+    lstm = tf.keras.models.load_model(model)
+    yhat = lstm.predict(X, verbose=0)
+
+    return yhat
+
+def rescale():
+    """
+    Rescale predictions so that unknown predictions are not included
+    """
+
+def apply_thresholds():
+    """
+    Apply thresholds to determine which predictions to report
+    """
