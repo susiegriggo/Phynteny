66377c6050f2a7c5ce464a06f8fc105b151e98b6
susiegriggo <susie.grigson@flinders.edu.au>
2023-01-25 15:08:42 +1030

    making predictions

--- phynteny.py
+++ phynteny.py
@@ -22,8 +22,10 @@ parser = argparse.ArgumentParser(description='Phynteny: synteny-based annotation
 parser = argparse.add_argument('infile', type=is_valid_file, help='input file in genbank format')
 parser.add_argument('-o', '--outfile', action='store', default=sys.stdout, type=argparse.FileType('w'),
                     help='where to write the output genbank file')
-parser.add_argument('-m', '--model', action='store', help='Path to custom LSTM model', deafult="link to model")
-parser.add_argument('-t', '--thresholds', action='store', help='Path to dictionaries for a custom LSTM model', default = 'link to the model thresholds')
+parser.add_argument('-m', '--model', action='store', help='Path to custom LSTM model',
+                    default='model/all_chunk_trained_LSTMbest_val_loss.h5')
+parser.add_argument('-t', '--thresholds', action='store', help='Path to dictionaries for a custom LSTM model',
+                    default='model/all_chunk_trained_LSTMbest_val_loss_thresholds.pkl')
 parser.add_argument('-V', '--version', action='version', version=__version__)
 
 args = parser.parse_args()
@@ -37,14 +39,17 @@ if not gb_dict:
 # ---------------------------------------------------
 
 # format the genbank file - file to make predictions
-this_phage = format_data.extract_features(gb_dict)
-data = format_data.generate_prediction(this_phage)
+input = format_data.extract_features(gb_dict)
+data = format_data.generate_prediction(input)
 
-# use lstm model to make some predicitions
-predictions = predict.predict(data,)
+# use lstm model to make some predictions
+yhat = predict.predict(data, args['model'])
+
+# use thresholds to determine which predictions to keep
+predictions = predict.threshold(yhat, args['thresholds'])
 
 # update the existing phage with the predictions
-
+out_dict = handle_genbank.add_predictions(gb_dict, predictions)
 
 # write output to a genbank file
-handle_genbank.write_genbank(this_phage, args['outfile'])
+handle_genbank.write_genbank(out_dict, args['outfile'])
