8901a9ca5181b9f8dedb49e77514d3345011dd24
susie.griggo <susie.grigson@flinders.edu.au>
2023-01-29 21:29:16 +1030

    check mmseqs file is not emtpy

--- phynteny/merge_phrog_annotations.py
+++ phynteny/merge_phrog_annotations.py
@@ -1,70 +1,72 @@
 """ 
 Add mmseqs annotations into phispy annotations 
-""" 
+"""
 
-#imports 
+# imports
 import glob2
-from Bio import SeqIO, bgzf 
+from Bio import SeqIO, bgzf
 import re
 import gzip
 import handle_genbank
 
-#read through each genome
-directories = glob2.glob('/home/edwa0468/phage/Prophage/phispy/phispy/GCA/' + '/*',  recursive=True)
-
+# read through each genome
+directories = glob2.glob('/home/edwa0468/phage/Prophage/phispy/phispy/GCA/' + '/*', recursive=True)
 
 for d in directories:
 
     e = glob2.glob(d + '/**', recursive=True)
-    zipped_gdict = [i for i in e if i[-6:] == 'gbk.gz'] 
+    zipped_gdict = [i for i in e if i[-6:] == 'gbk.gz']
 
     for file in zipped_gdict:
- 
+
         # convert genbank to a dictionary
         gb_dict = handle_genbank.get_genbank(file)
         gb_keys = list(gb_dict.keys())
 
-        file_parts = re.split('/',file)
+        file_parts = re.split('/', file)
         genbank_parts = re.split('_', file_parts[11])
-        mmseqs = '/home/edwa0468/phage/Prophage/phispy/phispy_phrogs/GCA/' + file_parts[8] + '/' + file_parts[9] + '/' + file_parts[10]
-        mmseqs_fetch= glob2.glob(mmseqs + '/*')
+        mmseqs = '/home/edwa0468/phage/Prophage/phispy/phispy_phrogs/GCA/' + file_parts[8] + '/' + file_parts[9] + '/' + \
+                 file_parts[10]
+        mmseqs_fetch = glob2.glob(mmseqs + '/*')
 
-        if len(mmseqs_fetch) > 0: 
+        if len(mmseqs_fetch) > 0:
 
-            #if os.path.getsize(filename):
-            annotations  = handle_genbank.get_mmseqs(mmseqs_fetch[0])
-            
-            if len(annotations) > 0: 
-                
-                phrogs = handle_genbank.filter_mmseqs(annotations)
-                genbank_name = '/home/grig0076/scratch/phispy_phrogs/GCA/' +  file_parts[8] + '/' + file_parts[9] + '/' + file_parts[10] + '/'  + genbank_parts[0] + '_' + genbank_parts[1] + '_phrogs_' + genbank_parts[3]
+            if os.path.getsize(mmseqs_fetch[0]):
+                annotations = handle_genbank.get_mmseqs(mmseqs_fetch[0])
 
-                #write to the genbank file
-                print(genbank_name, flush = True)
-                with gzip.open(genbank_name, 'wt') as handle:
-                
-                    #loop through each prophage
-                    for key in gb_keys: 
+                if len(annotations) > 0:
 
-                        #get a prophage
-                        this_prophage = gb_dict.get(key) 
+                    phrogs = handle_genbank.filter_mmseqs(annotations)
+                    genbank_name = '/home/grig0076/scratch/phispy_phrogs/GCA/' + file_parts[8] + '/' + file_parts[
+                        9] + '/' + file_parts[10] + '/' + genbank_parts[0] + '_' + genbank_parts[1] + '_phrogs_' + \
+                                   genbank_parts[3]
 
-                        #get the cds 
-                        cds = [i for i in this_prophage.features if i.type == 'CDS']
+                    # write to the genbank file
+                    print(genbank_name, flush=True)
+                    with gzip.open(genbank_name, 'wt') as handle:
 
-                        #loop through each protein
-                        for c in cds: 
+                        # loop through each prophage
+                        for key in gb_keys:
 
-                            if 'protein_id' in c.qualifiers.keys(): 
+                            # get a prophage
+                            this_prophage = gb_dict.get(key)
 
-                                pid = c.qualifiers.get('protein_id')
+                            # get the cds
+                            cds = [i for i in this_prophage.features if i.type == 'CDS']
 
-                                if phrogs.get(pid[0]) == None: 
-                                    c.qualifiers['phrog'] = 'No_PHROG'
-                                else: 
-                                    c.qualifiers['phrog'] = phrogs.get(pid[0])
+                            # loop through each protein
+                            for c in cds:
 
-                        #write genbank file 
-                        SeqIO.write(gb_dict.get(key), handle, 'genbank')
+                                if 'protein_id' in c.qualifiers.keys():
 
-                handle.close() 
+                                    pid = c.qualifiers.get('protein_id')
+
+                                    if phrogs.get(pid[0]) == None:
+                                        c.qualifiers['phrog'] = 'No_PHROG'
+                                    else:
+                                        c.qualifiers['phrog'] = phrogs.get(pid[0])
+
+                            # write genbank file
+                            SeqIO.write(gb_dict.get(key), handle, 'genbank')
+
+                    handle.close()
