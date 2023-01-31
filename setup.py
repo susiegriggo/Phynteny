e5192ca4e05ace0e422b40635e011c515667c26f
susiegriggo <susie.grigson@flinders.edu.au>
2023-01-31 16:20:24 +1030

    generate module

--- setup.py
+++ setup.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python3
-# encoding: utf-8
-# copy this from metacoag 
-from setuptools import setup, find_packages 
 
-def readme(): 
-    with open("README.md", "r") as fh:
-        long_desc = fh.read() 
-    return long_desc 
-
-def get_version(): 
-    with open("VERSION", "r") as f: 
-        v = f.readline().strip()
-        return v 
-
-def main(): 
-    setup(name='Phynteny',
-        version=get_version(),
-        long_description=readme(),
-        description='Synteny-based bacteriophage annotation',
-        url='https://github.com/susiegriggo/Phynteny',
-        author='Susanna Grigson',
-        scripts=['phynteny.py'],
-        packages=find_packages(),
-        entry_points={"console_scripts" : ["phynteny=phynteny.__main__:main"]},
-        include_package_data=True, 
-        author_email='susie.grigson@flinders.edu.au',
-        license='MIT'
-        )
+import setuptools
 
 
-if __name__ == "__main__":
-    main()
-    print(find_packages()) 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages = setuptools.find_packages()
+package_data = {"phynteny": ["phynteny/*"]}
+
+data_files = [(".", ["LICENSE", "README.md"])]
+
+setuptools.setup(
+    name="Phynteny",
+    version="0",
+    zip_safe=True,
+    author="Susanna Grigson",
+    author_email="susie.grigson@gmail.com",
+    description="Phynteny: Synteny-based prediction of bacteriophage genes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/susiegriggo/Phynteny",
+    license="MIT",
+    packages=packages,
+    package_data=package_data,
+    data_files=data_files,
+    include_package_data=True,
+    scripts=["phynteny.py"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[ #TODO
+        "biopython",
+        "tensorflow",
+        "pickle",
+        "pandas",
+        "numpy",
+    ],
+    python_requires=">=3.7",
+)
