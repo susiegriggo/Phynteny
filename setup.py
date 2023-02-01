386b2448becfd7977e0975e072164c2b8d990fb7
Susie Grigson <susie.grigson@flinders.edu.au>
2023-02-01 11:39:19 +1030

    restore to argparse

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
     install_requires=[ #TODO
         "biopython", 
         "tensorflow-cpu==2.11.0",
-        "pickle",
+        "pandas",
         "click", 
     ],
     python_requires=">=3.8",
