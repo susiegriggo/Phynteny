cbd179a4f919fd9d6dac274f5fe8350bce56e29d
susiegriggo <susie.grigson@flinders.edu.au>
2023-03-03 14:02:18 +1030

    click

--- scripts/generate_training_data.py
+++ scripts/generate_training_data.py
@@ -190,4 +190,4 @@ def main(input_data, output, maximum_genes, gene_categories, prefix):
     test_train(data, prefix, num_functions, maximum_genes)
 
 if __name__ == "__main__":
-    main()
+    main(input_data, output, maximum_genes, gene_categories, prefix)
