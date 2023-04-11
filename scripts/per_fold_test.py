cb730e3e3a122ea591ca128bbf48880cdd72c5c5
susiegriggo <susie.grigson@gmail.com>
2023-04-11 15:53:39 +0930

    write script to evaluate each fold of a model

---
+++ scripts/per_fold_test.py
@@ -0,0 +1,61 @@
+"""
+Performance testing for each k-fold separately
+"""
+
+
+# imports
+from phynteny_utils import statistics
+import click
+import tensorflow as tf
+import pickle5
+import numpy as np
+import glob
+import pkg_resources
+from sklearn.metrics import classification_report
+
+# get args
+@click.command()
+@click.option('--base', '-b', type=click.Path(), help='base to where the model paths are located. All k-folds should be placed in the same directory')
+@click.option('-x', type=click.Path(), help='file path to the testing data X components')
+@click.option('-y', type=click.Path(), help='file path to the testing data y components')
+@click.option('--out', '-o', type=click.Path(), help='output file path')
+
+def main(base, x, y, out):
+
+    # fetch the different PHROG categories
+    category_path =  pkg_resources.resource_filename("phynteny_utils", "phrog_annotation_info/integer_category.pkl")
+    category_names = pickle5.load(open(category_path, 'rb'))
+
+    # fetch the testing data
+    test_X = pickle5.load(open(x, 'rb'))
+    test_X = list(test_X.values())
+    test_y = pickle5.load(open(y, 'rb'))
+    test_y = list(test_y.values())
+
+    # fetch the models
+    files = glob.glob(base + '\*')
+    models = [tf.keras.models.load_model(f) for f in files]
+
+    # loop through each of the provided models
+    for i in range(len(models)):
+
+        # make predictions
+        scores = statistics.predict_softmax(test_X, len(category_names), models[i])
+        known_categories = statistics.known_category(test_X, test_y, len(category_names))
+
+        # build the ROC curve for this data
+        ROC_df = statistics.build_roc(scores, known_categories, category_names)
+        ROC_df.to_csv(out + 'ROC.tsv', sep = '\t')
+
+        # compute the classification report
+        report = classification_report(known_categories, [np.argmax(i) for i in scores], output_dict=True)
+        with open(out + 'report.tsv', "wb") as f:
+            pickle5.dump(report, f)
+
+        # calculate the AUC for each category
+        auc = statistics.per_category_auc(scores, known_categories, category_names)
+        with open(out + 'AUC.pkl', "wb") as f:
+            pickle5.dump(auc, f)
+
+if __name__ == "__main__":
+    main()
