17bf90ad91ea35ddde5003faf1022e92e2f7785b
susiegriggo <susie.grigson@gmail.com>
2023-05-01 22:42:39 +0930

    add click

--- scripts/merge_genbank_mmseqs.py
+++ scripts/merge_genbank_mmseqs.py
@@ -5,36 +5,40 @@ Script to merge mmseqs annotations with a genbank file
 from phynteny_utils import handle_genbank
 from Bio import SeqIO
 import gzip
+import click
+@click.command()
+@click.option("--genbank", "-g",  type=click.Path(), help="genbank file")
+@click.option("--mmseqs", "-m",  type=click.Path(), help="mmseqs file")
+@click.option("--out", "-o",  type=click.Path(), help="output genbank file")
 
-genbank = ''
-mmseqs = ''
-out_name = ''
+def main(genbank, mmseqs, out):
+    gb_dict = handle_genbank.get_genbank(genbank)
+    gb_keys = gb_dict.keys()
 
-gb_dict = handle_genbank.get_genbank(genbank)
-gb_keys = gb_dict.keys()
+    annotations = handle_genbank.get_mmseqs(mmseqs)
+    phrogs = handle_genbank.filter_mmseqs(annotations)
 
-annotations = handle_genbank.get_mmseqs(mmseqs)
-phrogs = handle_genbank.filter_mmseqs(annotations)
+    with gzip.open(out, 'wt') as handle:
 
-with gzip.open(out_name, 'wt') as handle:
+        for k in gb_keys:
 
-    for k in gb_keys:
+            this_phage = gb_keys.get(k)
+            cds = [i for i in this_phage.features if i.type == 'CDS']
+            for c in cds:
 
-        this_phage = gb_keys.get(k)
-        cds = [i for i in this_phage.features if i.type == 'CDS']
-        for c in cds:
+                if 'protein_id' in c.qualifiers.keys():
 
-            if 'protein_id' in c.qualifiers.keys():
+                    pid = c.qualifiers.get('protein_id')
 
-                pid = c.qualifiers.get('protein_id')
+                    if phrogs.get(pid[0]) == None:
+                        c.qualifiers['phrog'] = 'No_PHROG'
+                    else:
+                        c.qualifiers['phrog'] = phrogs.get(pid[0])
 
-                if phrogs.get(pid[0]) == None:
-                    c.qualifiers['phrog'] = 'No_PHROG'
-                else:
-                    c.qualifiers['phrog'] = phrogs.get(pid[0])
+            # write genbank file
+            SeqIO.write(gb_dict.get(k), handle, 'genbank')
 
-        # write genbank file
-        SeqIO.write(gb_dict.get(k), handle, 'genbank')
+    handle.close()
 
-handle.close()
-
+if __name__ == "__main__":
+    main()
