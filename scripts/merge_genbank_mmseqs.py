b9680c063efa7a4ac990a64c1a355cff8b0e4ae6
Susie Grigson <susie.grigson@flinders.edu.au>
2023-05-02 14:49:52 +0930

    change names

--- scripts/merge_genbank_mmseqs.py
+++ scripts/merge_genbank_mmseqs.py
@@ -1,40 +1,56 @@
 """
 Script to merge mmseqs annotations with a genbank file
 """
+
 # imports
 from phynteny_utils import handle_genbank
 from Bio import SeqIO
 import gzip
 import click
+
 @click.command()
 @click.option("--genbank", "-g",  type=click.Path(), help="genbank file")
-@click.option("--mmseqs", "-m",  type=click.Path(), help="mmseqs file")
 @click.option("--out", "-o",  type=click.Path(), help="output genbank file")
 
-def main(genbank, mmseqs, out):
+def main(genbank, out):
+    emax = 1e-10     
+    print('reading genbank', flush = True) 
     gb_dict = handle_genbank.get_genbank(genbank)
     gb_keys = gb_dict.keys()
 
-    annotations = handle_genbank.get_mmseqs(mmseqs)
-    phrogs = handle_genbank.filter_mmseqs(annotations)
+    print('getting mmseqs', flush = True) 
+    print('looping', flush = True)
 
     with gzip.open(out, 'wt') as handle:
 
+
+        #loop through the genome 
         for k in gb_keys:
 
-            this_phage = gb_keys.get(k)
-            cds = [i for i in this_phage.features if i.type == 'CDS']
-            for c in cds:
+            this_phage = gb_dict.get(k)
+            features  = [i for i in this_phage.features]
 
-                if 'protein_id' in c.qualifiers.keys():
 
-                    pid = c.qualifiers.get('protein_id')
+            #loop through features in the genome 
+            for f in features:
 
-                    if phrogs.get(pid[0]) == None:
-                        c.qualifiers['phrog'] = 'No_PHROG'
-                    else:
-                        c.qualifiers['phrog'] = phrogs.get(pid[0])
+                #change type to gene 
+                f.type = 'CDS' 
+                 
+                if 'phrog_id' in f.qualifiers.keys():
 
+        
+                    if float(f.qualifiers['evalue'][0]) < emax: 
+                        f.qualifiers['phrog'] = f.qualifiers['phrog_id'][0][6:]
+                    else: 
+                        f.qualifiers['phrog'] = 'No_PHROG'
+
+                else: 
+
+                    f.qualifiers['phrog'] = 'No_PHROG' 
+
+
+            # add an evalue filtering step here as well 
             # write genbank file
             SeqIO.write(gb_dict.get(k), handle, 'genbank')
 
