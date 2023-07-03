cc17a232c3eae867dc0adda9451363ef31bc2139
susiegriggo <susie.grigson@gmail.com>
2023-07-03 11:55:46 +0930

    add sequence to the table

--- phynteny_utils/predictor.py
+++ phynteny_utils/predictor.py
@@ -126,17 +126,22 @@ def generate_table(outfile, gb_dict, categories, phrog_integer):
     # convert annotations made to a text file
     with click.open_file(outfile, "wt") if outfile != ".tsv" else sys.stdout as f:
         f.write(
-            "ID\tstart\tend\tstrand\tphrog_id\tphrog_category\tphynteny_category\tphynteny_score\tconfidence\ttranslation\tphage\n"
+            "ID\tstart\tend\tstrand\tphrog_id\tphrog_category\tphynteny_category\tphynteny_score\tconfidence\tsequence\tphage\n"
         )
 
         for k in keys:
-            # print(k, flush=True)
+
+            # obtain the sequence
+            seq = gb_dict.get(k).seq
+
+            # get the genes
             cds = [f for f in gb_dict.get(k).features if f.type == "CDS"]
 
             # extract the features for the cds
             start = [c.location.start for c in cds]
             end = [c.location.end for c in cds]
-            translation = [c.qualifiers.get('translation')[0] for c in cds]
+            seq = [str(seq[start[i]:end[i]]) for i in range(len(cds))]
+
             strand = [c.strand for c in cds]
             ID = [
                 c.qualifiers.get("ID")[0] if "ID" in c.qualifiers else "" for c in cds
@@ -178,7 +183,7 @@ def generate_table(outfile, gb_dict, categories, phrog_integer):
             # write to table
             for i in range(len(cds)):
                 f.write(
-                    f"{ID[i]}\t{start[i]}\t{end[i]}\t{strand[i]}\t{phrog[i]}\t{known_category[i]}\t{phynteny_category[i]}\t{phynteny_score[i]}\t{phynteny_confidence[i]}\t{translation[i]}\t{k}\n"
+                    f"{ID[i]}\t{start[i]}\t{end[i]}\t{strand[i]}\t{phrog[i]}\t{known_category[i]}\t{phynteny_category[i]}\t{phynteny_score[i]}\t{phynteny_confidence[i]}\t{seq[i]}\t{k}\n"
                 )
 
     return found
