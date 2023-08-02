fix softmax bug

phynteny_utils/statistics.py
@@ -109,7 +109,7 @@ def predict_softmax(X_encodings, num_categories, model):
 
     # obtain softmax scores for the masked genes
     X_encodings = np.array(X_encodings)
-    yhat = model(X_encodings, training=False)
+    yhat = model.predict(X_encodings) #TODO line which needs investigating 
     scores_list = yhat
 
     return np.array(scores_list)